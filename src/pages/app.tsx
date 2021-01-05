5e92d467d6bc859883c3961720c1e1a95cf6a1b5
thephilgray <thephilgray@gmail.com>
2021-01-04 20:26:25 -0500

    add new routes, add not found page for auth users

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -5,25 +5,29 @@ import { Router } from '@reach/router'
 
 import Assignment from '../components/Assignments/Assignment'
 import Profile from '../components/Profile/Profile'
-import Home from '../components/Home'
 import Layout from '../components/Layout/Layout'
 import NewAssignment from '../components/Assignments/NewAssignment'
 import NewSubmission from '../components/Submissions/NewSubmission'
 import PrivateRoute from '../components/Auth/PrivateRoute'
 import Submissions from '../components/Submissions/Submissions'
 import Submission from '../components/Submissions/Submission'
+import NewFileRequestLink from '../components/NewFileRequest'
+import Uploads from '../components/Submissions/Uploads'
+import NotFound from '../components/NotFound'
 
 const App: React.FC = (): JSX.Element => (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Layout>
             <Router>
-                <PrivateRoute path="/app" component={Home} />
+                <NotFound default />
+                <PrivateRoute path="/app" component={NewFileRequestLink} />
                 <PrivateRoute path="/app/profile" component={Profile} />
                 <PrivateRoute path="/app/assignments/new" component={NewAssignment} />
                 <PrivateRoute path="/app/assignments/:assignmentId" component={Assignment} />
                 <PrivateRoute path="/app/assignments/:assignmentId/submissions" component={Submissions} />
                 <PrivateRoute path="/app/assignments/:assignmentId/submissions/new" component={NewSubmission} />
                 <PrivateRoute path="/app/assignments/:assignmentId/submissions/:submissionId" component={Submission} />
+                <Uploads path="/app/uploads/:fileRequestId" />
             </Router>
         </Layout>
     </MuiPickersUtilsProvider>
