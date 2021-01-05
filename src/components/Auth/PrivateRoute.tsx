4d67ba36e1a1395458540d688213e16afe19f2cf
thephilgray <thephilgray@gmail.com>
2021-01-04 21:27:10 -0500

    return null on private routes if not authenticated

--- src/components/Auth/PrivateRoute.tsx
+++ src/components/Auth/PrivateRoute.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import { RouteComponentProps } from '@reach/router'
+import { navigate } from 'gatsby'
 import { isLoggedIn } from '../../utils/auth'
-import { RouteComponentProps, navigate } from '@reach/router'
 
 interface PrivateRouteProps extends RouteComponentProps {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     component: any
 }
 
-const PrivateRoute = (props: PrivateRouteProps): JSX.Element => {
+const PrivateRoute = (props: PrivateRouteProps): JSX.Element | null => {
     // eslint-disable-next-line react/prop-types
     const { component: Component, ...rest } = props
 
     if (!isLoggedIn()) {
         navigate(`/signin`)
+        return null
     }
     return <Component {...rest} />
 }
