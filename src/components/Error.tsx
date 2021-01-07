b6f39c6bb8b26c0f9c5db7b3ee4ed863658f11b6
thephilgray <thephilgray@gmail.com>
2021-01-06 21:55:55 -0500

    remove cognito link from Error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -7,10 +7,6 @@ const Error: React.FC<{ [prop: string]: unknown }> = (props) => (
             <pre key={err}>
                 <strong>{err}: </strong>
                 {JSON.stringify(val, null, ' ')}
-                <br />
-                <a href="https://docs.aws.amazon.com/cognito/latest/developerguide/user-pool-settings-attributes.html">
-                    AWS Cognito User Pool documentation.
-                </a>
             </pre>
         ))}
     </div>
