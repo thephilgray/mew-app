f9458eaedf94df2a80f0b3ce8de3f4741d9956bb
thephilgray <thephilgray@gmail.com>
2023-05-27 00:38:47 -0400

    style the admin view toggle group, remove unused imports

--- src/components/AdminViewToggle.tsx
+++ src/components/AdminViewToggle.tsx
@@ -1,9 +1,7 @@
 import { AdminPanelSettings, Person } from '@mui/icons-material'
-import { Grid, ToggleButton, ToggleButtonGroup } from '@mui/material'
+import { Grid, Paper, ToggleButton, ToggleButtonGroup } from '@mui/material'
 import React from 'react'
 import { useIsAdmin, useViewAdmin } from '../auth/hooks'
-import GroupGuard from './Auth/GroupGuard'
-import { Group } from '../constants'
 import If from './If'
 
 
@@ -13,17 +11,22 @@ export default function AdminViewToggle() {
   return (
     <If condition={isAdmin}>
       <Grid container>
-        <Grid item xs={12} sx={{ pb: 2 }}>
-          <ToggleButtonGroup
-            exclusive
-            value={!!viewAdmin ? "admin" : "member"}
-            onChange={(e, value) =>
-              setViewAdmin(value === "admin")}
-            sx={{ float: "right" }}>
-            { }
-            <ToggleButton value="member" aria-label="Member View">Member View <Person /></ToggleButton>
-            <ToggleButton value="admin" aria-label="Admin View">Admin View <AdminPanelSettings /></ToggleButton>
-          </ToggleButtonGroup>
+        <Grid item xs={12} sx={{ pb: 2, display: 'flex', justifyContent: 'end' }}>
+          <Paper sx={{ height: '100%', width: 'fit-content' }}>
+            <fieldset>
+              <legend style={{ textAlign: 'center' }}>admin view toggle</legend>
+              <ToggleButtonGroup
+                exclusive
+                value={!!viewAdmin ? "admin" : "member"}
+                onChange={(e, value) =>
+                  setViewAdmin(value === "admin")}
+                sx={{ float: "right" }}>
+                { }
+                <ToggleButton value="member" aria-label="Member View">Member View <Person /></ToggleButton>
+                <ToggleButton value="admin" aria-label="Admin View">Admin View <AdminPanelSettings /></ToggleButton>
+              </ToggleButtonGroup>
+            </fieldset>
+          </Paper>
         </Grid>
       </Grid>
     </If>
