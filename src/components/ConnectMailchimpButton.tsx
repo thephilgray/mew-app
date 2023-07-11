471eba4036c4633175b9b7e2bfa94e6a880a32fb
thephilgray <thephilgray@gmail.com>
2023-07-10 20:51:27 -0400

    add warning to mailchimp connect button that unsaved changes will be lost

--- src/components/ConnectMailchimpButton.tsx
+++ src/components/ConnectMailchimpButton.tsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress, Icon } from '@mui/material'
+import { Box, Button, CircularProgress, Grid, Icon, Typography } from '@mui/material'
 import { Link } from 'gatsby'
 import React from 'react'
 import mailchimp from '../assets/mailchimp.svg'
@@ -6,6 +6,7 @@ import { useLocation } from 'react-use'
 import { updateMembershipService } from '../graphql/mutations'
 import { gql, useMutation } from '@apollo/react-hooks'
 import { useProfile } from '../auth/hooks'
+import { Dangerous, Warning } from '@mui/icons-material'
 
 const MCLogo = () => (
   <Icon>
@@ -35,18 +36,25 @@ export default function ConnectMailchimpButton({ mailchimpEnabled = false, works
   }
 
   const ConnectMailchimp = () => (
-    <Button
-      sx={{
-        backgroundColor: "#ffe01b", color: "text.primary", '&:hover': {
-          backgroundColor: "rgba(255,224,27,0.65)"
-        },
-      }}
-      variant="contained"
-      startIcon={connectLoading ? <CircularProgress size="1rem" color="inherit" /> : < MCLogo />}
-      component={Link}
-      href={`https://login.mailchimp.com/oauth2/authorize?response_type=code&client_id=${MAILCHIMP_CLIENT_ID}&redirect_uri=${OAUTH_CALLBACK}`}>
-      Connect Mailchimp
-    </Button >
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <Typography><Warning sx={{ verticalAlign: 'bottom' }} /> Warning: The following action will cause you to be navigated away to complete authentication. Unsaved changes will be lost. Save any changes first and then return to this page to connect.</Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          sx={{
+            backgroundColor: "#ffe01b", color: "text.primary", '&:hover': {
+              backgroundColor: "rgba(255,224,27,0.65)"
+            },
+          }}
+          variant="contained"
+          startIcon={connectLoading ? <CircularProgress size="1rem" color="inherit" /> : < MCLogo />}
+          component={Link}
+          href={`https://login.mailchimp.com/oauth2/authorize?response_type=code&client_id=${MAILCHIMP_CLIENT_ID}&redirect_uri=${OAUTH_CALLBACK}`}>
+          Connect Mailchimp
+        </Button >
+      </Grid>
+    </Grid>
   )
 
   const DisconnectMailchimp = () => (
