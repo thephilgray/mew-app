f6562dd3f944c2adc12553a3f1f163e7be6c9b8d
thephilgray <thephilgray@gmail.com>
2023-07-26 19:57:54 -0400

    fix wrapping of bio and lyrics

--- src/components/Playlists/Playlist.tsx
+++ src/components/Playlists/Playlist.tsx
@@ -428,7 +428,7 @@ const Playlist: React.FC<PropsWithChildren<RouteComponentProps<{ assignmentId: s
                 </Grid >
                 <If condition={!!audioLists?.[currentIndex]?.lyrics}></If>
                 <Grid item xs={12} sx={{ maxWidth: '90vw', overflow: 'auto' }}>
-                    <pre>
+                    <pre style={{ whiteSpace: "pre-wrap", wordWrap: "break-word" }}>
                         <Typography variant="body2">
                             {audioLists?.[currentIndex]?.lyrics}
                         </Typography>
